9c866f7aeba87e454eb4d5e2b32da203094544da
Maxim Grishaev <maxim.grishaev@gmail.com>
2023-12-24 22:05:16 +0100

    fix: template

--- src/d00/part2.rs
+++ src/d00/part2.rs
@@ -1,16 +1,15 @@
 use super::help;
 
-pub fn run_str(_str: &str) -> u128 {
-    todo!()
+pub fn run_str(_str: &str) -> usize {
+    0
 }
 
-pub fn run() -> u128 {
+pub fn run() -> usize {
     run_str(&help::read_data())
 }
 
 #[cfg(test)]
 mod tests {
-    use super::super::help::t;
     use super::*;
     // use insta::assert_debug_snapshot;
 
@@ -23,6 +22,6 @@ mod tests {
     #[ignore = "template"]
     #[test]
     fn test_run() {
-        assert_eq!(run_str(t::EXAMPLE.trim()), 0);
+        assert_eq!(run_str(help::t::EXAMPLE), 0);
     }
 }
